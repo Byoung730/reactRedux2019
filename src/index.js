8d652b0c0a0436ebba2bbb8045d7f550fc9d43e9
Brian <brian.young@maritz.com>
2019-04-11 15:30:21 -0500

    WIP

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter Name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        Submit
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
