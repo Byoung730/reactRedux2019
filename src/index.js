204b2a2f92c56b37435fc7fc563c0cbf5ed79f0c
Brian <brian.young@maritz.com>
2019-04-11 17:02:42 -0500

    props

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
-    <div>
-      <label class="label" for="name">
-        Enter Name:
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: "blue", color: "white" }}>
-        Submit
-      </button>
+    <div className="ui container comments">
+      <CommentDetail name="Tony" time="6:00PM" text="Hello" />
+      <CommentDetail name="Chris" time="7:00PM" text="How are you" />
+      <CommentDetail name="Pat" time="8:00PM" text="Goodbye" />
     </div>
   );
 };
