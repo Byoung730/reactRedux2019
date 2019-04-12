5c41d614349cbf0f0c8c982ac26fc5ca0c54ffc6
Brian <brian.young@maritz.com>
2019-04-12 15:09:02 -0500

    props.children

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments" style={{ paddingTop: "1rem" }}>
-      <CommentDetail name="Tony" time="6:00PM" text="Hello" date="4/6/19" />
-      <CommentDetail
-        name="Chris"
-        time="7:00PM"
-        text="How are you"
-        date="4/8/19"
-      />
-      <CommentDetail name="Pat" time="8:00PM" text="Goodbye" date="4/10/19" />
+      <ApprovalCard>
+        <CommentDetail name="Tony" time="6:00PM" text="Hello" date="4/6/19" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          name="Chris"
+          time="7:00PM"
+          text="How are you"
+          date="4/8/19"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail name="Pat" time="8:00PM" text="Goodbye" date="4/10/19" />
+      </ApprovalCard>
     </div>
   );
 };
